626564b062b400b93821ece0ca71c7ef5ff7658e
Wissle <jongsome@gmail.com>
2025-10-21 22:31:16 +0900

    feat: Make toggle buttons visible only on screen cast take.

--- extension.js
+++ extension.js
@@ -27,31 +27,44 @@ export default class ScreencastWithAudio extends Extension {
         this._screenshotUI = Main.screenshotUI;
         this._showPointerButtonContainer = this._screenshotUI._showPointerButtonContainer;
         this._showPointerButton = this._screenshotUI._showPointerButton;
+        this._shotButton = this._screenshotUI._shotButton;
 
         // Created widgets
         this._desktopAudioButton = new St.Button({
           style_class: 'screenshot-ui-show-pointer-button',
           icon_name: 'audio-speakers-symbolic',
           toggle_mode: true,
+          visible: ! this._shotButton.checked
         });
         this._micAudioButton = new St.Button({
           style_class: 'screenshot-ui-show-pointer-button',
           icon_name: 'audio-input-microphone-symbolic',
           toggle_mode: true,
+          visible: ! this._shotButton.checked
         });
 
         // Add widgets
-        this._showPointerButtonContainer.insert_child_above(
+        this._showPointerButtonContainer.insert_child_at_index(
           this._desktopAudioButton,
-          this._showPointerButton
+          0
         );
-        this._showPointerButtonContainer.insert_child_above(
+        this._showPointerButtonContainer.insert_child_at_index(
           this._micAudioButton,
-          this._showPointerButton
+          1
+        );
+
+        // connect to signals.
+        this._shotButtonNotifyChecked = this._shotButton.connect (
+          'notify::checked',
+          (_object, _pspec) => {
+            this._desktopAudioButton.visible = ! this._shotButton.checked
+            this._micAudioButton.visible = ! this._shotButton.checked
+          }
         );
     }
 
     disable() {
+        this._shotButton.disconnect(this._shotButtonNotifyChecked);
         this._showPointerButtonContainer.remove_child(this._desktopAudioButton);
         this._showPointerButtonContainer.remove_child(this._micAudioButton);
     }
